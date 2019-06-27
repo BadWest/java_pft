dc38221c39d558523064e94a9aa274d55ffb4b3c
BadWest <35765042+BadWest@users.noreply.github.com>
2019-06-27 19:28:14 +0300

    Point

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,29 @@
+package ru.stqa.pft.sandbox;
+
+class Point {
+  private static double x;
+  private static double y;
+
+  public String toString() {
+    return "("+x+";"+y+")";
+  }
+  public Point() {
+    x = 0.0;
+    y = 0.0;
+  }
+  public Point(double a, double b) {
+    x = a;
+    y = b;
+  }
+  public static double distance(Point p) {
+    return Math.sqrt( Math.pow(p.x-x,2) + Math.pow(p.y-y,2) );
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    Point p1 = new Point();
+    Point p2 = new Point(1,1);
+    System.out.println("Растстояние между точками "+p1+" и "+p2+" равно "+p1.distance(p2));
+  }
+}

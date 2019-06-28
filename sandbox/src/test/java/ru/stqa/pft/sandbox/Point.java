0c8e5b072a659c68da63290fa4b80f6d08d0d270
BadWest <35765042+BadWest@users.noreply.github.com>
2019-06-28 14:13:04 +0300

    Добавлены изменения в части лекции

--- sandbox/src/test/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/Point.java
@@ -20,10 +20,3 @@ class Point {
   }
 }
 
-class Main {
-  public static void main(String[] args) {
-    Point p1 = new Point();
-    Point p2 = new Point(1,1);
-    System.out.println("Растстояние между точками "+p1+" и "+p2+" равно "+p1.distance(p2));
-  }
-}
